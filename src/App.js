7fbdc51fece238c80f8bce66b91b174c0ad24460
jfsaenzr <47581366+jfsaenzr@users.noreply.github.com>
2022-06-09 15:49:44 -0500

    *

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import './App.css';
-import logo from './imagenes/logo.png'
+import logo from './imagenes/logo.png';
+import Boton from './componentes/Boton'
 
 function App() {
   return (
     <div className="App">
-
       <div className='logo-contenedor'>
-
         <img
           src={logo}
           className='logo'
           alt='Logo'
         />
-
       </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
 
+
+        </div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
